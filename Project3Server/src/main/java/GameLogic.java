4c7f1dfab87e35d1c852c0cb8954f296a21a1f4a
Esteban Sanz <estebanzcol@gmail.com>
2025-04-23 21:33:38 -0500

    Adding the game logic code now

--- Project3Server/src/main/java/GameLogic.java
+++ Project3Server/src/main/java/GameLogic.java
@@ -2,9 +2,31 @@ public class GameLogic {
     private static final int col = 7;
     private static final int row = 6;
     int[][] Grid;
+    int counter = 0;
 
 
-    public void fillBoard(){
+    //I made it an int so we can represent... 0 = invalid move ... 1 = valid move ... 2 = won game
+    public int makeMove(int colCoor, int playerNum){
+        if(Grid == null){
+            fillInitialBoard();
+        }
+        if(colCoor > 6 || colCoor < 0){ // this should never be the case but just in case
+            return 0;
+        }
+        if(!canPlace(colCoor)){
+            return 0;
+        }
+        for(int x = 0; x < row; ++x){
+            if(Grid[x][colCoor] != -1){
+                Grid[x-1][colCoor] = playerNum;
+            }
+        }
+        if(ifWin(playerNum)){
+            return 2;
+        }
+        return 1; //I cant think of another case sooooo if you do just add it
+    }
+    public void fillInitialBoard(){
         Grid = new int[6][7];
         for(int x = 0; x < row; ++x){
             for(int y = 0; y < col; ++y){
@@ -12,11 +34,90 @@ public class GameLogic {
             }
         }
     }
-    public boolean canPlace(int xCoor){
+    public boolean canPlace(int colCoor){
         if(Grid == null){
-            fillBoard();
+            fillInitialBoard();
         }
-
+        if(Grid[0][colCoor] == -1){
+            return true;
+        }
+        return false;
+    }
+    public boolean ifWin(int playerNum){
+        if(Grid == null){
+            fillInitialBoard();
+        }
+        if(checkDiagonal(playerNum)){
+            return true;
+        }
+        else if(checkHorizontal(playerNum)){
+            return true;
+        }
+        else if(checkVertical(playerNum)){
+            return true;
+        }
+        return false;
+    }
+    //x represents row number and y represents column number because it is a 6x7 grid
+    public boolean checkDiagonal(int playerNum){
+        //bottom left to top right diagonal
+        for(int x = 3; x < row; ++x) {
+            for(int y = 0; y < col-3; ++y){
+                if(Grid[x][y] == playerNum &&
+                        Grid[x-1][y+1] == playerNum &&
+                        Grid[x-2][y+2] == playerNum &&
+                        Grid[x-3][y+3] == playerNum){
+                    return true;
+                }
+            }
+        }
+        //top left to bottom right diagonal
+        for(int x = 0; x < row-3; ++x) {
+            for(int y = 0; y < col-3; ++y){
+                if(Grid[x][y] == playerNum &&
+                        Grid[x+1][y+1] == playerNum &&
+                        Grid[x+2][y+2] == playerNum &&
+                        Grid[x+3][y+3] == playerNum){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean checkVertical(int playerNum){
+        for(int x = 0; x < row; ++x){
+            for(int y = 0; y < col; ++y){
+                if(Grid[x][y] == playerNum){
+                    ++counter;
+                }
+                else{
+                    counter = 0;
+                }
+                if(counter == 4){
+                    counter = 0;
+                    return true;
+                }
+            }
+        }
+        counter = 0;
+        return false;
+    }
+    public boolean checkHorizontal(int playerNum){
+        for(int y = 0; y < col; ++y){
+            for(int x = 0; x < row; ++x){
+                if(Grid[x][y] == playerNum){
+                    ++counter;
+                }
+                else{
+                    counter = 0;
+                }
+                if(counter == 4){
+                    counter = 0;
+                    return true;
+                }
+            }
+        }
+        counter = 0;
         return false;
     }
 }
